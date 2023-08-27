89b2596c6eea4c074d72f1e609aba9661051647c
Teja Goud Kandula <kandulateja6@gmail.com>
2023-08-27 13:11:17 +0530

    Exercise 14 complete

---
+++ Exercise13/ex13.py
@@ -0,0 +1,8 @@
+from sys import argv
+# read the WYSS sectioin for how to ru this : WYSS -> What you Should see
+script, first, second, third = argv
+
+print("The script is called:", script)
+print("Your first variable is:", first)
+print("Your second varialbe is:", second)
+print("Your third variable is:", third)
